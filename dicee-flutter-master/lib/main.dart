47b8af185a38dc51da535b3c187bf5c776de3592
hirenvadher95 <47621081+hirenvadher95@users.noreply.github.com>
2019-07-12 22:28:11 +0530

    Initial commit

---
+++ dicee-flutter-master/lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dice'),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNum = 1;
+  int rightDicNum = 1;
+  void getRandomNuM() {
+    setState(() {
+      rightDicNum = Random().nextInt(6) + 1;
+      leftDiceNum = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+              child: FlatButton(
+                  onPressed: () {
+                    getRandomNuM();
+                  },
+                  child: Image.asset("images/dice$leftDiceNum.png"))),
+          Expanded(
+              child: FlatButton(
+                  onPressed: () {
+                    getRandomNuM();
+                  },
+                  child: Image.asset("images/dice$rightDicNum.png"))),
+        ],
+      ),
+    );
+  }
+}
